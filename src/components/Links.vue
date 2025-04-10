571bcd6c400028df220fb485f6a9683d1142f16f
cysnies <cysnies@outlook.com>
2025-04-10 17:13:01 +0800

    feat: update site links

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -45,7 +45,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, Download, Code } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, Download, Code, Android, Windows} from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -63,6 +63,8 @@ const siteIcon = {
   Fire,
   Download,
   Code,
+  Android,
+  Windows
 };
 
 // 计算分组后的网站链接
